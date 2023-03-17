b4782a370a555d2eb11feff5762ab44d9ee7ac68
AltiMario <altimario@gmail.com>
2023-03-17 15:04:22 +0000

    migration generic

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,25 +1,21 @@
 const FlightSuretyApp = artifacts.require("FlightSuretyApp");
 const FlightSuretyData = artifacts.require("FlightSuretyData");
-const fs = require('fs');
+const fs = require("fs");
 
-
-module.exports = function(deployer) {
-    
-    let firstAirline = "0xe2DF2Bb2417e070c30C402158aDe89a6D78337b1";
-    deployer.deploy(FlightSuretyData, firstAirline)
-    .then( () => {
-        return deployer.deploy(FlightSuretyApp, FlightSuretyData.address)
-                .then(async () => {
-                    let config = {
-                        localhost: {
-                            url: 'http://localhost:8545',
-                            dataAddress: FlightSuretyData.address,
-                            appAddress: FlightSuretyApp.address
-                        }
-                    }
-                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                    fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                    
-                });
+module.exports = function (deployer, network, accounts) {
+    deployer.deploy(FlightSuretyData, accounts[0]).then(() => {
+        return deployer
+            .deploy(FlightSuretyApp, FlightSuretyData.address)
+            .then(() => {
+                let config = {
+                    localhost: {
+                        url: "http://localhost:7545",
+                        dataAddress: FlightSuretyData.address,
+                        appAddress: FlightSuretyApp.address,
+                    },
+                };
+                fs.writeFileSync(__dirname + "/../src/dapp/config.json",JSON.stringify(config, null, "\t"),"utf-8");
+                fs.writeFileSync(__dirname + "/../src/server/config.json",JSON.stringify(config, null, "\t"),"utf-8");
+            });
     });
-}
+};
